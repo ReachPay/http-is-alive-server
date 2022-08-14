5ffb9297e5dec46857cc182cdd0f3c041479ad5f
amigin <amigin@gmail.com>
2022-08-14 21:06:18 +0300

    Updated Logger as singletone

--- src/start_up.rs
+++ src/start_up.rs
@@ -2,16 +2,15 @@ use std::{net::SocketAddr, sync::Arc};
 
 use is_alive_middleware::IsAliveMiddleware;
 use my_http_server::MyHttpServer;
-use rust_extensions::{ApplicationStates, Logger};
+use rust_extensions::ApplicationStates;
 
 pub fn start_server(
     app_name: String,
     app_version: String,
     app_states: Arc<dyn ApplicationStates + Send + Sync + 'static>,
-    logger: Arc<dyn Logger + Send + Sync + 'static>,
 ) {
     let mut http_server = MyHttpServer::new(SocketAddr::from(([0, 0, 0, 0], 8000)));
 
     http_server.add_middleware(Arc::new(IsAliveMiddleware::new(app_name, app_version)));
-    http_server.start(app_states, logger);
+    http_server.start(app_states);
 }
